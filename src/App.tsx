9da4249933aea28290e1636d52be9696a440c638
Manu254rce <straussmoabit@gmail.com>
2022-06-08 00:46:56 +0300

    changes v0.1.0:2

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@ import React from 'react'
 function App() {
   return (
     <div className='aurora-outer'>
-      <div className='aurora-inner'>
-        <h1 className='text'>Best IDE</h1>
+      <div className='aurora-middle'>
+        <div className='aurora-inner'>
+          <h1 className='text-white text-right font-adventpro mr-16 text-7xl mt-32'>
+            Best IDE
+          </h1>
+          <h3 className='text-white text-right font-novaflat mr-16 text-lg mt-5'>version 0.1.0</h3>
+        </div>
       </div>
     </div>
   )
---
+++ tailwind.config.js
@@ -0,0 +1,54 @@
+const colors = require('tailwindcss/colors')
+const plugin  = require('tailwindcss/plugin')
+
+
+module.exports = {
+  mode: 'jit',
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}'
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors:
+      {
+        transparent: 'transparent',
+        currentColor: 'currentColor',
+        primary: '#6600cc',
+        secondary: '#00ff55',
+        'sky-blue': '#33ffff',
+      },
+
+      textShadow:
+      {
+        default: '0px 2px 8px var(--tw-shadow-color)',
+        sm: '0px 0px 2px var(--tw-shadow-color)',
+        md: '0px 0px 8px var(--tw-shadow-color)',
+        lg: '0px 0px 16px var(--tw-shadow-color)',
+      },
+
+      fontFamily:
+      {
+        'adventpro': '"Advent Pro"',
+        'zrnic': 'Zrnic',
+        'audiowide': 'Audiowide',
+        'novaflat': '"Nova Flat"',
+      },
+    },
+  },
+  plugins: [
+    require("tailwindcss-scrollbar"),
+    require("tailwindcss-textshadow"),
+
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
+}
