547dd19be2202e7407cd1bf6af153915fc37a16e
Manu254rce <straussmoabit@gmail.com>
2022-06-08 00:56:26 +0300

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,26 @@ function App() {
     <div className='aurora-outer'>
       <div className='aurora-middle'>
         <div className='aurora-inner'>
-          <h1 className='text-white text-right font-adventpro mr-16 text-7xl mt-32'>
-            Best IDE
-          </h1>
-          <h3 className='text-white text-right font-novaflat mr-16 text-lg mt-5'>version 0.1.0</h3>
+          <Intro/>
+          <hr className='line1'/>
         </div>
       </div>
     </div>
+  )
+}
+
+const Intro = () => {
+  return (
+    <>
+      <h1 className='text-white text-right font-adventpro
+                     mr-16 text-7xl mt-28'>
+        Best IDE
+      </h1>
+      <h3 className='text-white text-right font-novaflat 
+                      mr-16 text-lg mt-4 italic'>
+        version 0.1.0
+      </h3>
+    </>
   )
 }
 
