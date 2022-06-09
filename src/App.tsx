1f5f4b33b5a18d4cad2252e64174f46205205b99
Manu254rce <straussmoabit@gmail.com>
2022-06-09 21:13:08 +0300

    changes v0.1.0:3

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,69 @@ function App() {
     <div className='aurora-outer'>
       <div className='aurora-middle'>
         <div className='aurora-inner'>
-          <Intro/>
-          <hr className='line1'/>
+            <Welcome/>
         </div>
       </div>
     </div>
   )
 }
 
+const Welcome = () => {
+  return (
+    <section className='absolute right-0 top-0 backdrop-brightness-75
+                        h-full w-2/3'>
+      <Intro/>  
+        <hr className='line1' />
+      <Options/>
+    </section>
+    
+  )
+}
+
 const Intro = () => {
   return (
     <>
-      <h1 className='text-white text-right font-adventpro
-                     mr-16 text-7xl mt-28'>
-        Best IDE
+      <h1 className='text-black text-right font-adventpro
+                     mr-16 text-5xl mt-32'>
+        Code and building. Extra refined
       </h1>
-      <h3 className='text-white text-right font-novaflat 
-                      mr-16 text-lg mt-4 italic'>
-        version 0.1.0
+      <h3 className='text-black text-right font-novaflat 
+                      mr-16 text-sm mt-6 italic'>
+        An Integrated Development Environment, founded upon 
+        <span className='text-primary 
+                        text-shadow-md shadow-primary'> speed</span> , 
+        <span className='text-primary 
+                        text-shadow-md shadow-primary'> light</span> and 
+        <span className='text-primary 
+                        text-shadow-md shadow-primary'> time</span>
       </h3>
     </>
+  )
+}
+
+const Options = () => {
+  return (
+    <div className='rounded-md flex flex-col mr-20 mt-14
+                    w-1/3 h-1/3 mx-auto drop-shadow-2xl'>
+      <button className='projectOps'>
+        <img alt='newProject' 
+            src='../icons/clarity-assets-master/icons/essential/plus-circle-line.svg' 
+            width={30}/>
+        <span className='mx-auto'>New Project</span>
+      </button>
+      <button className='projectOps'>
+        <img alt='newProject'
+          src='../icons/clarity-assets-master/icons/core/folder-open-line.svg'
+          width={30} />
+        <span className='mx-auto'>Open Project</span>
+      </button>
+      <button className='projectOps group'>
+        <img alt='newProject'
+          src='../icons/clarity-assets-master/icons/technology/rack-server-line.svg'
+          width={30} />
+        <span className='mx-auto'>Open from VCS</span>
+      </button>
+    </div>
   )
 }
 
