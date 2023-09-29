777e64bfb7873b431c34143df0833c3d4098785a
Manu254rce <straussmoabit@gmail.com>
2023-09-29 15:55:20 +0300

    New changes

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,33 +1,22 @@
-
-#![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
-)]
-
-// use tauri::Manager;
-// // Create the command:
-// // This command must be async so that it doesn't run on the main thread.
-// #[tauri::command]
-// async fn close_splashscreen(window: tauri::Window) {
-//   // Close splashscreen
-//   if let Some(splashscreen) = window.get_window("splashscreen") {
-//     splashscreen.close().unwrap();
-//   }
-//   // Show main window
-//   window.get_window("main").unwrap().show().unwrap();
-// }
-
-
+use tauri::{Manager, SystemTray, SystemTrayEvent, SystemTrayMenu};
 
 fn main() {
-  tauri::Builder::default()
-
-    // .invoke_handler(tauri::generate_handler![close_splashscreen])
-    // .run(tauri::generate_context!())
-    // .expect("failed to run app");
-     // configure the menu
-
-
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    let system_tray_menu = SystemTrayMenu::new();
+    tauri::Builder::default()
+        .system_tray(SystemTray::new().with_menu(system_tray_menu))
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::LeftClick { position: _, size: _ } => {
+                let window = app.get_window("main").unwrap();
+                // toggle application window
+                if window.is_visible().unwrap() {
+                    window.hide().unwrap();
+                } else {
+                    window.show().unwrap();
+                    window.set_focus().unwrap();
+                }
+            }
+            _ => {}
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
